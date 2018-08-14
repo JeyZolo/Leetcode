172c28194d8eed83dd3141f763f99854c9359d61
JeyZolo <bread888@gmail.com>
2018-08-13 22:50:17 -0400

    56,138. Subarray Sum,349. Intersection of Two Arrays

--- meregrIntervals.java
+++ meregrIntervals.java
@@ -132,3 +132,36 @@ public class Solution {
         return result;
     }
 }
+/*138. Subarray Sum
+Given an integer array, find a subarray where the sum of numbers is zero. Your code should return the index of the first number and the index of the last number.
+
+Example
+Given [-3, 1, 2, -3, 4], return [0, 2] or [1, 3].
+
+Notice
+There is at least one subarray that it's sum equals to zero.*/
+public class Solution {
+    /**
+     * @param nums: A list of integers
+     * @return: A list of integers includes the index of the first number and the index of the last number
+     */
+    public List<Integer> subarraySum(int[] nums) {
+        // use hashmap
+        HashMap<Integer,Integer> map = new HashMap<>();
+        List<Integer> res = new ArrayList<>();
+        int sum = 0;
+        map.put(0, -1);
+        for (int i = 0; i < nums.length; i++) {
+             sum += nums[i];
+            if (map.containsKey(sum)){
+                res.add(map.get(sum) + 1);
+                res.add(i);
+                break;
+            }
+            else {
+                map.put(sum, i);
+            }
+        }
+        return res;
+    }
+}
