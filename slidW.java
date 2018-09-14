74651a16fa73742d9878b19120c333c9467bdeee
JeyZolo <bread888@gmail.com>
2018-09-13 23:56:44 -0400

    u pre

--- slidW.java
+++ slidW.java
@@ -472,3 +472,61 @@ class Solution {
         return weighted;
     }
 }
+/*
+103. Binary Tree Zigzag Level Order Traversal
+DescriptionHintsSubmissionsDiscussSolution
+Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its zigzag level order traversal as:
+[
+  [3],
+  [20,9],
+  [15,7]
+]*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null)
+            return res;
+        //bfs the tree
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int level = 0;
+        
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> sublist = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if(level % 2 == 0)
+                    sublist.add(node.val);
+                else sublist.add(0,node.val);
+                
+                if(node.left != null)
+                    queue.offer(node.left);
+                if(node.right != null)
+                    queue.offer(node.right);  
+            }
+            level++;
+            res.add(new ArrayList<>(sublist));
+            //direction = direction == true? false: true;
+        }
+        return res;
+    }
+}
